5d316abe67a64b742633f5da515e8f969a16a853
fodela <particleme30@gmail.com>
2022-01-11 18:04:11 +0000

    added the sticky navigation functionality

--- js/script.js
+++ js/script.js
@@ -31,10 +31,32 @@ allLinks.forEach(function (link) {
     }
     //Close mobile navigation
     if (link.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open ");
+      headerEl.classList.toggle("nav-open");
     }
   });
 });
+
+/////////////////////////////////////////////////////////// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 
